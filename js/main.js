d8936d3d4fcf53356ad9e31cc4bd710f95fcb3c4
Md Ishaq Ibrahim <mdishaqibrohim@gmail.com>
2022-03-22 05:24:33 +0600

    2dn commit

--- js/main.js
+++ js/main.js
@@ -21,113 +21,92 @@ $("[data-background]").each(function(){
 });
 
 // One Page Nav
-var top_offset = $('.header-area').height() - 10;
-$('.main-menu nav ul').onePageNav({
-	currentClass: 'active',
-	scrollOffset: top_offset,
-});
+// var top_offset = $('.header-area').height() - 10;
+// $('.main-menu nav ul').onePageNav({
+// 	currentClass: 'active',
+// 	scrollOffset: top_offset,
+// });
 
 
-$(window).on('scroll', function () {
-	var scroll = $(window).scrollTop();
-	if (scroll < 245) {
-		$(".header-sticky").removeClass("sticky");
-	} else {
-		$(".header-sticky").addClass("sticky");
-	}
-});
+// $(window).on('scroll', function () {
+// 	var scroll = $(window).scrollTop();
+// 	if (scroll < 245) {
+// 		$(".header-sticky").removeClass("sticky");
+// 	} else {
+// 		$(".header-sticky").addClass("sticky");
+// 	}
+// });
 
 
 
 // mainSlider
-function mainSlider() {
-	var BasicSlider = $('.slider-active');
-	BasicSlider.on('init', function (e, slick) {
-		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-		doAnimations($firstAnimatingElements);
-	});
-	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-		doAnimations($animatingElements);
-	});
-	BasicSlider.slick({
-		autoplay: false,
-		autoplaySpeed: 10000,
-		dots: false,
-		fade: true,
-		arrows: false,
-		responsive: [
-			{ breakpoint: 767, settings: { dots: false, arrows: false } }
-		]
-	});
+// function mainSlider() {
+// 	var BasicSlider = $('.slider-active');
+// 	BasicSlider.on('init', function (e, slick) {
+// 		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+// 		doAnimations($firstAnimatingElements);
+// 	});
+// 	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+// 		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+// 		doAnimations($animatingElements);
+// 	});
+// 	BasicSlider.slick({
+// 		autoplay: false,
+// 		autoplaySpeed: 10000,
+// 		dots: false,
+// 		fade: true,
+// 		arrows: false,
+// 		responsive: [
+// 			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+// 		]
+// 	});
 
-	function doAnimations(elements) {
-		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		elements.each(function () {
-			var $this = $(this);
-			var $animationDelay = $this.data('delay');
-			var $animationType = 'animated ' + $this.data('animation');
-			$this.css({
-				'animation-delay': $animationDelay,
-				'-webkit-animation-delay': $animationDelay
-			});
-			$this.addClass($animationType).one(animationEndEvents, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-}
-mainSlider();
+// 	function doAnimations(elements) {
+// 		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+// 		elements.each(function () {
+// 			var $this = $(this);
+// 			var $animationDelay = $this.data('delay');
+// 			var $animationType = 'animated ' + $this.data('animation');
+// 			$this.css({
+// 				'animation-delay': $animationDelay,
+// 				'-webkit-animation-delay': $animationDelay
+// 			});
+// 			$this.addClass($animationType).one(animationEndEvents, function () {
+// 				$this.removeClass($animationType);
+// 			});
+// 		});
+// 	}
+// }
+// mainSlider();
 
-
-// owlCarousel
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-	items:1,
-	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-    nav:true,
-	dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        767:{
-            items:3
-        },
-        992:{
-            items:5
-        }
-    }
-})
 
 
 /* magnificPopup img view */
-$('.popup-image').magnificPopup({
-	type: 'image',
-	gallery: {
-	  enabled: true
-	}
-});
+// $('.popup-image').magnificPopup({
+// 	type: 'image',
+// 	gallery: {
+// 	  enabled: true
+// 	}
+// });
 
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-	type: 'iframe'
-});
+// /* magnificPopup video view */
+// $('.popup-video').magnificPopup({
+// 	type: 'iframe'
+// });
 
 
 // isotop
-$('.grid').imagesLoaded( function() {
-	// init Isotope
-	var $grid = $('.grid').isotope({
-	  itemSelector: '.grid-item',
-	  percentPosition: true,
-	  masonry: {
-		// use outer width of grid-sizer for columnWidth
-		columnWidth: '.grid-item',
-	  }
-	});
-});
+// $('.grid').imagesLoaded( function() {
+// 	// init Isotope
+// 	var $grid = $('.grid').isotope({
+// 	  itemSelector: '.grid-item',
+// 	  percentPosition: true,
+// 	  masonry: {
+// 		// use outer width of grid-sizer for columnWidth
+// 		columnWidth: '.grid-item',
+// 	  }
+// 	});
+// });
 
 // filter items on button click
 $('.portfolio-menu').on( 'click', 'button', function() {
@@ -145,20 +124,20 @@ $('.portfolio-menu button').on('click', function(event) {
 
 
 
-// scrollToTop
-$.scrollUp({
-	scrollName: 'scrollUp', // Element ID
-	topDistance: '300', // Distance from top before showing element (px)
-	topSpeed: 300, // Speed back to top (ms)
-	animation: 'fade', // Fade, slide, none
-	animationInSpeed: 200, // Animation in speed (ms)
-	animationOutSpeed: 200, // Animation out speed (ms)
-	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
-	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-});
+// // scrollToTop
+// $.scrollUp({
+// 	scrollName: 'scrollUp', // Element ID
+// 	topDistance: '300', // Distance from top before showing element (px)
+// 	topSpeed: 300, // Speed back to top (ms)
+// 	animation: 'fade', // Fade, slide, none
+// 	animationInSpeed: 200, // Animation in speed (ms)
+// 	animationOutSpeed: 200, // Animation out speed (ms)
+// 	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+// 	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+// });
 
-// WOW active
-new WOW().init();
+// // WOW active
+// new WOW().init();
 
 
 })(jQuery);
